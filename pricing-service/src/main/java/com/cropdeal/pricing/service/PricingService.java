5729f902ff290aa34819cb9282656c98c3941f38
srimansk <s.k.sriman007@gmail.com>
2026-09-19 22:42:11 +0530

    Delete pricing-service directory

--- pricing-service/src/main/java/com/cropdeal/pricing/service/PricingService.java
+++
@@ -1,9 +0,0 @@
-package com.cropdeal.pricing.service;
-
-import com.cropdeal.pricing.dto.PricingRequest;
-import com.cropdeal.pricing.dto.PricingResponse;
-
-public interface PricingService {
-
-    PricingResponse calculateFairPrice(PricingRequest request);
-}
